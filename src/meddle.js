7fb94d8bfd503e1511cdd1a2c2ca167962f59ecf
Elisha Cook <elisha@elishacook.com>
2014-06-25 13:19:23 -0400

    Created Records

--- src/meddle.js
+++ src/meddle.js
@@ -4,7 +4,17 @@
 
 var Scalar = function (defaultValue)
 {
-    this._value = defaultValue === undefined ? null : defaultValue
+    Object.defineProperty(this, '_value', {
+        writable: true,
+        enumerable: false,
+        value: defaultValue === undefined ? null : defaultValue
+    })
+    
+    Object.defineProperty(this, 'onchange', {
+        writable: true,
+        enumerable: false,
+        value: undefined
+    })
 }
 Scalar.prototype = 
 {
@@ -22,23 +32,164 @@ Scalar.prototype =
         {
             this.onchange(oldValue, value)
         }
-    },
-    
-    onchange: null
+    }
 }
 
 
-var Model = 
+var List = function (arr)
 {
-    create: function ()
+    Object.defineProperty(this, 'onchange', {
+        writable: true,
+        enumerable: false,
+        value: undefined
+    })
+    
+    if (arr)
     {
-        return {}
+        Array.prototype.push.apply(this, arr)
+    }
+}
+List.prototype = Object.create(Array.prototype, (function ()
+{
+    var props = 
+    {
+        remove:
+        {
+            enumerable: false,
+            value: function ()
+            {
+                for (var i=0; i<arguments.length; i++)
+                {
+                    var index = this.indexOf(arguments[i])
+                    
+                    if (-1 < index)
+                    {
+                        this.splice(index, 1)
+                    }
+                }
+            }
+        }
+    }
+    
+    var mutators = [
+        'fill',
+        'pop',
+        'push',
+        'reverse',
+        'shift',
+        'sort',
+        'splice',
+        'unshift'
+    ]
+    
+    mutators.forEach(function (mutatorName)
+    {
+        if (Array.prototype[mutatorName] === undefined)
+        {
+            return
+        }
+        
+        props[mutatorName] = 
+        {
+            enumerable: false,
+            value: function ()
+            {
+                var retVal = Array.prototype[mutatorName].apply(this, arguments)
+                
+                if (this.onchange)
+                {
+                    var args = Array.prototype.slice.call(arguments)
+                    args.unshift(mutatorName)
+                    this.onchange.apply(undefined, args)
+                }
+                
+                return retVal
+            }
+        }
+    })
+    
+    return props
+})())
+
+var Record = function (attrs)
+{
+    var RecordClass = function ()
+    {
+        for (var n in attrs)
+        {
+            var attr = attrs[n]()
+            attr.onchange = this.createChangeListener(n)
+            
+            if (attrs[n].type == Scalar)
+            {
+                this.createProperty(n, attr)
+            }
+            else
+            {
+                this[n] = attr
+            }
+        }
+    }
+    RecordClass.prototype = 
+    {
+        createProperty: function (name, scalar)
+        {
+            Object.defineProperty(this, name, 
+            {
+                get: function ()
+                {
+                    return scalar.get() 
+                },
+                
+                set: function (value)
+                {
+                    return scalar.set(value)
+                }
+            })
+        },
+        
+        createChangeListener: function (name)
+        {
+            return this.changeListener.bind(this, name)
+        },
+        
+        changeListener: function ()
+        {
+            if (this.onchange)
+            {
+                this.onchange.apply(undefined, arguments)
+            }
+        }
+    }
+    
+    for (var n in attrs)
+    {
+        RecordClass[n] = attrs[n]
+    }
+    
+    return RecordClass
+}
+
+var makeFactory = function (constructor)
+{
+    return function ()
+    {
+        var args = Array.prototype.slice.call(arguments, 0),
+            factory = function ()
+            {
+                var inst = Object.create(constructor.prototype)
+                constructor.apply(inst, args)
+                return inst
+            }
+        factory.type = constructor
+        return factory
     }
 }
 
 var Meddle = {
-    Scalar: Scalar,
-    Model: Model
+    Scalar: makeFactory(Scalar),
+    List: makeFactory(List),
+    Record: Record
 }
 
 window.Meddle = Meddle
--- src/meddle.spec.js
+++ src/meddle.spec.js
@@ -18,14 +18,14 @@ describe("Meddle", function ()
         
         it("has a get method", function ()
         {
-            var foo = new Meddle.Scalar()
+            var foo = Meddle.Scalar()()
             expect(foo.set).toBeDefined()
             expect(foo.set instanceof Function).toBeTruthy()
         })
         
         it("has a default value of null", function ()
         {
-            var foo = new Meddle.Scalar(),
+            var foo = Meddle.Scalar()(),
                 val = foo.get()
                 
             expect(val).toBeDefined()
@@ -34,48 +34,166 @@ describe("Meddle", function ()
         
         it("has a set method", function ()
         {
-            var foo = new Meddle.Scalar()
+            var foo = Meddle.Scalar()()
             expect(foo.set).toBeDefined()
             expect(foo.set instanceof Function).toBeTruthy()
         })
         
         it("can have its value set", function ()
         {
-            var foo = new Meddle.Scalar()
+            var foo = Meddle.Scalar()()
             foo.set('bar')
             expect(foo.get()).toEqual('bar')
         })
         
         it("can have a default value", function ()
         {
-            var foo = new Meddle.Scalar(23)
+            var foo = Meddle.Scalar(23)()
             expect(foo.get()).toEqual(23)
         })
         
-        it("will notify a listener when value is set", function ()
+        it("notifies a change listener when value is set", function ()
         {
-            var foo = new Meddle.Scalar()
+            var foo = Meddle.Scalar()()
             foo.onchange = stubFunctions.onchange
             foo.set(777)
             expect(stubFunctions.onchange).toHaveBeenCalled()
         })
         
-        it("will provide the old and new values to the change listener", function ()
+        it("provides the old and new values to the change listener", function ()
         {
-            var foo = new Meddle.Scalar(666)
+            var foo = Meddle.Scalar(666)()
             foo.onchange = stubFunctions.onchange
             foo.set(777)
             expect(stubFunctions.onchange).toHaveBeenCalledWith(666, 777)
         })
     })
+
+    describe("List", function ()
+    {
+        it("can have items added to it", function ()
+        {
+            var foos = Meddle.List()()
+            foos.push(1, 2, 3)
+            expect(foos).toEqual([1,2,3])
+        })
+        
+        it("can have items removed from it", function ()
+        {
+            var foos = Meddle.List()()
+            foos.push(1, 2, 3)
+            foos.remove(2, 1)
+            expect(foos).toEqual([3])
+        })
+        
+        it("can do other array things", function ()
+        {
+            var foos = Meddle.List()()
+            foos.push(1,2,3)
+            var squaredArray = foos.map(function (x) { return x * x })
+            expect(squaredArray).toEqual([1,4,9])
+        })
+        
+        it("can be initialized with an existing array", function ()
+        {
+            var foos = Meddle.List([1,2,3])()
+            expect(foos).toEqual([1,2,3])
+        })
+        
+        it("notifies a change listener when modified", function ()
+        {
+            var mutatingFunctions = {
+                    fill: [0],
+                    pop: [],
+                    push: [1],
+                    reverse: [],
+                    shift: [],
+                    sort: [],
+                    splice: [0,1],
+                    unshift: [1]
+                }
+            
+            for (var n in mutatingFunctions)
+            {
+                var spy = jasmine.createSpy(n+"ChangeListener"),
+                    foo = Meddle.List([1,2,3])()
+                    
+                foo.onchange = spy
+                
+                if (foo[n] !== undefined)
+                {
+                    foo[n].apply(foo, mutatingFunctions[n])
+                    expect(spy).toHaveBeenCalled()
+                }
+            }
+        })
+        
+        it("passes the modification method name and arguments to the change listener", function ()
+        {
+            var foo = Meddle.List([1,2,3])()
+            foo.onchange = jasmine.createSpy("changeListener")
+            foo.push(23)
+            expect(foo.onchange).toHaveBeenCalledWith('push', 23)
+        })
+    })
     
-    describe("Model", function ()
+    describe("Record", function ()
     {
         it("can create new classes", function ()
         {
-            var Something = Meddle.Model.create()
-            expect(Something).toBeDefined()
-            expect(Something).not.toBeNull()
+            var Foo = Meddle.Record()
+            expect(Foo).toBeDefined()
+            expect(Foo).not.toBeNull()
+        })
+        
+        it("can be initialized with a number of fields", function ()
+        {
+            var bar = Meddle.Scalar(123),
+                baz = Meddle.List([1,2,3]),
+                Foo = Meddle.Record({
+                    bar: bar,
+                    baz: baz
+                })
+            expect(Foo.bar).toBe(bar)
+            expect(Foo.baz).toBe(baz)
+        })
+        
+        it("can generate instances", function ()
+        {
+            var Foo = Meddle.Record({
+                    bar: Meddle.Scalar(123),
+                    baz: Meddle.List([1,2,3])
+                }),
+                f = new Foo()
+                
+            expect(f.bar).toEqual(123)
+            expect(f.baz).toEqual([1,2,3])
+        })
+        
+        it("has a change listener that fires when a scalar property changes", function ()
+        {
+            var Foo = Meddle.Record({
+                    bar: Meddle.Scalar(123),
+                    baz: Meddle.List([1,2,3])
+                }),
+                f = new Foo()
+                
+            f.onchange = jasmine.createSpy('onchange')
+            f.bar = 666
+            expect(f.onchange).toHaveBeenCalledWith('bar', 123, 666)
+        })
+        
+        it("has a change listener that fires when a list property changes", function ()
+        {
+            var Foo = Meddle.Record({
+                    bar: Meddle.Scalar(123),
+                    baz: Meddle.List([1,2,3])
+                }),
+                f = new Foo()
+                
+            f.onchange = jasmine.createSpy('onchange')
+            f.baz.push('purple carrot')
+            expect(f.onchange).toHaveBeenCalledWith('baz', 'push', 'purple carrot')
         })
     })
 })
